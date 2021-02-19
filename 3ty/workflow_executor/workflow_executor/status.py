1064a6a5104b1d0ea49953c0efb8b452c073ff10
bbrauzzi <blasco.brauzzi@terradue.com>
2021-02-19 12:59:50 +0100

    debug mode added

--- 3ty/workflow_executor/workflow_executor/status.py
+++ 3ty/workflow_executor/workflow_executor/status.py
@@ -9,9 +9,12 @@ from workflow_executor import helpers
 
 def run(namespace, workflow_name, state=None):
 
+    debugmode= True
     # create an instance of the API class
     apiclient = helpers.get_api_client()
     api_instance = client.BatchV1Api(api_client=apiclient)
+    core_v1 = client.CoreV1Api(api_client=apiclient)
+
 
     pretty = True
 
@@ -29,7 +32,23 @@ def run(namespace, workflow_name, state=None):
             pprint(status)
             return status
         elif api_response.status.failed:
-            status = {"status": "Failed", "error": api_response.status.conditions[0].message}
+            if debugmode:
+
+                controllerUid = api_response.metadata.labels["controller-uid"]
+                pod_label_selector = "controller-uid=" + controllerUid
+                pods_list = core_v1.list_namespaced_pod(namespace=namespace, label_selector=pod_label_selector, timeout_seconds=10)
+                pod_name = pods_list.items[0].metadata.name
+                try:
+                    # For whatever reason the response returns only the first few characters unless
+                    # the call is for `_return_http_data_only=True, _preload_content=False`
+                    pod_log_response = core_v1.read_namespaced_pod_log(name=pod_name, namespace=namespace, _return_http_data_only=True, _preload_content=False)
+                    pod_log = pod_log_response.data.decode("utf-8")
+                    status = {"status": "Failed", "error": pod_log }
+                except client.rest.ApiException as e:
+                    status = {"status": "Failed", "error": api_response.status.conditions[0].message}
+
+            else:    
+                status = {"status": "Failed", "error": api_response.status.conditions[0].message}
             pprint(status)
             return status
 
